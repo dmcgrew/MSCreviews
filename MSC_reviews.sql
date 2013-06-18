ef43f21677a2b2106d7ddc354c360d0d8a559640
Dustin McGrew <dustinmcgrew@me.com>
2013-06-18 17:07:46 -0400

    new db

--- MSC_reviews.sql
+++ MSC_reviews.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 17, 2013 at 11:34 PM
+-- Generation Time: Jun 18, 2013 at 05:07 PM
 -- Server version: 5.5.9
--- PHP Version: 5.2.17
+-- PHP Version: 5.3.6
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
@@ -28,14 +28,24 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 CREATE TABLE `reviews` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL COMMENT 'The user id the review belongs to.',
-  `rating` int(11) NOT NULL,
+  `kpa1` text NOT NULL,
+  `kp1_rating` int(11) NOT NULL,
+  `kpa1_notes` text NOT NULL,
+  `date_updated` datetime NOT NULL,
+  `date_created` datetime NOT NULL,
+  `date_published` datetime NOT NULL,
+  `published` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Dumping data for table `reviews`
 --
 
+INSERT INTO `reviews` VALUES(1, 1, '', 0, '', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0);
+INSERT INTO `reviews` VALUES(2, 15, '', 0, '', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0);
+INSERT INTO `reviews` VALUES(3, 12, '', 0, '', '2013-06-18 13:15:48', '0000-00-00 00:00:00', '2013-06-18 13:20:02', 1);
+INSERT INTO `reviews` VALUES(4, 12, '', 0, '', '2013-06-18 13:17:19', '0000-00-00 00:00:00', '2013-06-18 13:19:48', 1);
 
 -- --------------------------------------------------------
 
