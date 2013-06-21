c4b05e82b498b8cfe1a6147fc6925f48250a5283
Dustin McGrew <dustinmcgrew@me.com>
2013-06-21 17:00:14 -0400

    add ion auth

--- MSC_reviews.sql
+++ MSC_reviews.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 19, 2013 at 05:04 PM
+-- Generation Time: Jun 21, 2013 at 04:59 PM
 -- Server version: 5.5.9
 -- PHP Version: 5.3.6
 
@@ -45,6 +45,52 @@ INSERT INTO `employees` VALUES(2, 'jyavorsky', 'db7cbb5f80895c5f02af592e8caa62ac
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `groups`
+--
+
+CREATE TABLE `groups` (
+  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  `description` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=10 ;
+
+--
+-- Dumping data for table `groups`
+--
+
+INSERT INTO `groups` VALUES(1, 'admin', 'Administrator');
+INSERT INTO `groups` VALUES(2, 'members', 'General User');
+INSERT INTO `groups` VALUES(3, 'yavorsky', '');
+INSERT INTO `groups` VALUES(4, 'parkhill', '');
+INSERT INTO `groups` VALUES(5, 'merriman', '');
+INSERT INTO `groups` VALUES(6, 'peters', '');
+INSERT INTO `groups` VALUES(7, 'rooney', '');
+INSERT INTO `groups` VALUES(8, 'miller', '');
+INSERT INTO `groups` VALUES(9, 'metzger', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login_attempts`
+--
+
+CREATE TABLE `login_attempts` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `ip_address` varbinary(16) NOT NULL,
+  `login` varchar(100) NOT NULL,
+  `time` int(11) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `login_attempts`
+--
+
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `reviews`
 --
 
@@ -75,3 +121,72 @@ INSERT INTO `reviews` VALUES(34, 1, '<p>test time again fool</p>', 2, '', '2013-
 INSERT INTO `reviews` VALUES(35, 2, '<p>timmeeee</p>', 4, '', '2013-06-19 13:41:30', '2013-06-19 13:41:30', '2013-06-19 13:41:30', 1);
 INSERT INTO `reviews` VALUES(36, 1, '<p>review</p>', 4, '', '2013-06-19 13:46:19', '2013-06-19 13:46:19', '2013-06-19 13:46:19', 1);
 INSERT INTO `reviews` VALUES(37, 1, '<p>yyoyoyoyoyo</p>', 5, '', '2013-06-19 13:46:27', '2013-06-19 13:46:27', '2013-06-19 13:46:27', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `ip_address` varbinary(16) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(80) NOT NULL,
+  `salt` varchar(40) DEFAULT NULL,
+  `email` varchar(100) NOT NULL,
+  `activation_code` varchar(40) DEFAULT NULL,
+  `forgotten_password_code` varchar(40) DEFAULT NULL,
+  `forgotten_password_time` int(11) unsigned DEFAULT NULL,
+  `remember_code` varchar(40) DEFAULT NULL,
+  `created_on` int(11) unsigned NOT NULL,
+  `last_login` int(11) unsigned DEFAULT NULL,
+  `active` tinyint(1) unsigned DEFAULT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `company` varchar(100) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` VALUES(1, '\0\0', 'administrator', '59beecdf7fc966e2f17fd8f65a4a9aeb09d4a3d4', '9462e8eee0', 'admin@admin.com', '', NULL, NULL, '9d029802e28cd9c768e8e62277c0df49ec65c48c', 1268889823, 1371847050, 1, 'Admin', 'istrator', 'ADMIN', '0');
+INSERT INTO `users` VALUES(2, '\0\0', 'test test', '266fa460a330501762c2c323c270e0dd22469ba8', NULL, 'dmcgrew@marketspaceagency.com', NULL, NULL, NULL, NULL, 1371845010, 1371845066, 1, 'test', 'test', 'test', '0');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_groups`
+--
+
+CREATE TABLE `users_groups` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) unsigned NOT NULL,
+  `group_id` mediumint(8) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uc_users_groups` (`user_id`,`group_id`),
+  KEY `fk_users_groups_users1_idx` (`user_id`),
+  KEY `fk_users_groups_groups1_idx` (`group_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `users_groups`
+--
+
+INSERT INTO `users_groups` VALUES(1, 1, 1);
+INSERT INTO `users_groups` VALUES(2, 1, 2);
+INSERT INTO `users_groups` VALUES(3, 2, 2);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `users_groups`
+--
+ALTER TABLE `users_groups`
+  ADD CONSTRAINT `fk_users_groups_users1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_users_groups_groups1` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
